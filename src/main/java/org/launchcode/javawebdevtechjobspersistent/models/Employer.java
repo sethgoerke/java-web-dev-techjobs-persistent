9f117c14ef9cfe8721e92d12672cd4b25b7a5fd3
Seth Goerke <sethgoerke@hotmail.com>
2021-03-17 16:07:06 -0500

    up to controllers

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-public class Employer extends AbstractEntity {
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
+@Entity
+public class Employer extends AbstractEntity {
+//I added all below
+
+    @NotBlank(message = "Location is required")
+    private String location;
+
+    public Employer(){
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+//created everything above
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,14 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+
+    private String longerDescriptionOfSkill;
+
+    public Skill (){
+    }
 
 }
