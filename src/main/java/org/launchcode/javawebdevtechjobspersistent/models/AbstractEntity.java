26760ac6bc3267dd0b95f64e395df94aacd08d2f
Seth Goerke <sethgoerke@hotmail.com>
2021-03-17 13:59:53 -0500

    up to models

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min= 3, max=75, message= "Name must be between 3 and 75 characters")
     private String name;
 
     public int getId() {
